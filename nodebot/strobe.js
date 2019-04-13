5d59cdf70f7bd8ae661e04d1bbd887e0de739b4b
JohnWSMartel <jwm3464@g.rit.edu>
2019-04-13 08:57:48 -0400

    Save for bool idea

--- nodebot/strobe.js
+++ nodebot/strobe.js
@@ -1,11 +1,39 @@
 var five = require("johnny-five");
-var board = new five.Board();
+var board = new five.board();
+
+//bool to trigger when sensor changes
+var hit1 = false;
+//added line to account for multiple boards
+//var boards = new five.Boards(["A", "B"]);
 
 board.on("ready", function() {
+	
+	//added some code for when more boards are added
+	/*
+	//find boards by label
+	var led = new five.Led({
+		pin: 13,
+		board: this.byId("A")
+	});
+	
+	led.blink();
+	
+	this.each(function(board){
+		if(board.id === "B"){
+			//initialize an led instance on pin 13 of each initialized board and strobe it.
+			var led = new five.Led({
+				pin: 13,
+				board: board
+			});
+			led.blink();
+		}		
+	});
+	*/
   var sensor = new five.Sensor("A0");
   
   // Scale the sensor's data from 0-1023 to 0-10 and log changes
   sensor.on("change", function() {
     console.log(this.fscaleTo(0, 10));
+		hit1 = true;
   });
 });
