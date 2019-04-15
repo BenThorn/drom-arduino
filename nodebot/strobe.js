3a37bcc187098e72a8b9314f8b5a0b1096ab1e73
Ben Thorn <42948875+BenThorn@users.noreply.github.com>
2019-04-14 23:12:19 -0400

    Add files via upload

--- nodebot/strobe.js
+++ nodebot/strobe.js
@@ -1,43 +1,26 @@
 var five = require("johnny-five");
-var board = new five.board();
+var board = new five.Board();
+var robot = require("robotjs");
 
-//bool to trigger when sensor changes
-var hit1 = false;
-//added lines to account for multiples
-/*var boards = new five.Boards(["A", "B", "C"]);
-var hit2 = false;
-var hit3 = false;
-*/
-
+var numHits = 0;
+var newHit = true;
 
 board.on("ready", function() {
-	
-	//added some code for when more boards are added
-	/*
-	//find boards by label
-	var led = new five.Led({
-		pin: 13,
-		board: this.byId("A")
-	});
-	
-	led.blink();
-	
-	this.each(function(board){
-		if(board.id === "B"){
-			//initialize an led instance on pin 13 of each initialized board and strobe it.
-			var led = new five.Led({
-				pin: 13,
-				board: board
-			});
-			led.blink();
-		}		
-	});
-	*/
   var sensor = new five.Sensor("A0");
   
   // Scale the sensor's data from 0-1023 to 0-10 and log changes
   sensor.on("change", function() {
-    console.log(this.fscaleTo(0, 10));
-		hit1 = true;
+    robot.typeString(" ");
+   // console.log(this.fscaleTo(0, 10));
+
+    if(this.fscaleTo(0, 10) > 1 && newHit) {
+      newHit = false;
+      numHits++;
+      console.log(numHits);
+    }
+
+    if(this.fscaleTo(0, 10) <= 0.01) {
+      newHit = true;
+    }
   });
 });
